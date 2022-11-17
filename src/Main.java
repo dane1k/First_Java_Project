c207a1cc4007c6cb32d1ca359160f301050a6db2
dane1k <dan9.b0rdan@yandex.ru>
2022-11-17 21:59:23 +1300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner YAEBLAN = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName = YAEBLAN.nextLine();
+        System.out.println("Username is: " + userName);
+    }
+}
