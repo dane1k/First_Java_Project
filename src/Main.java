2415024cdd5687445a8982214e3d8bbf5d22dc57
danek <113758655+dane1k@users.noreply.github.com>
2023-05-25 15:04:26 +1200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
-        Scanner YAEBLAN = new Scanner(System.in);
+        Scanner Scanner1 = new Scanner(System.in);
         System.out.println("Enter username");
 
-        String userName = YAEBLAN.nextLine();
+        String userName = Scanner1.nextLine();
         System.out.println("Username is: " + userName);
     }
 }
